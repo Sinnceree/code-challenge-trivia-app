1acd5c929856df688218e0dcefb7ff47514e51cf
VoXzE <sinncere520@gmail.com>
2021-04-26 18:40:24 -0400

    feat: added fetching questions from api handler

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { QuestionObject } from "../../interfaces";
 import AnswersBox from "../AnswersBox";
 import BorderedButton from "../BorderedButton";
@@ -6,22 +6,21 @@ import CompletedBox from "../CompletedBox";
 import QuestionBox from "../QuestionBox";
 
 const Quiz = () => {
-  const [questions, setQuestions] = useState<QuestionObject[]>([{
-    "category": "Entertainment: Video Games",
-    "type": "boolean",
-    "difficulty": "hard",
-    "question": "In &quot;The Sims&quot; series, the most members in a household you can have is 8.",
-    "correct_answer": "True",
-    "incorrect_answers": [
-    "False"
-    ]
-    }]);
+  const [questions, setQuestions] = useState<QuestionObject[]>([]);
   const [currentQuestionIndex, setQuestionIndex] = useState<number>(0);
   const [selectedAnswer, setSelectedAnswer] = useState<"True" | "False" | null>(null);
   const [completed, setCompleted] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchQuestions = async () => {
+    const questionsRaw = await fetch("https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean");
+    const questionsData = await questionsRaw.json();
+    setQuestions(questionsData.results);
+    setLoading(false);
+
+  }
 
   const handleSelectedAnswer = (answer: "True" | "False" | null) => {
-    console.log(answer)
     setSelectedAnswer(answer)
   }
 
@@ -46,21 +45,24 @@ const Quiz = () => {
 
     setQuestionIndex(currentQuestionIndex + 1);
     setSelectedAnswer(null);
-
-    console.log(questions);
   }
+
+  // Fetch questions on component mount.
+  useEffect(() => {
+    fetchQuestions();
+  }, [])
 
   return (
     <div className="quiz-wrapper">
-      {!completed ? 
+      {!completed && !loading &&
         <React.Fragment>
           <QuestionBox question={questions[currentQuestionIndex].question} currentIndex={currentQuestionIndex + 1} />
           <AnswersBox handleSelectedAnswer={handleSelectedAnswer} selectedAnswer={selectedAnswer} />
           <BorderedButton text="Next Question" onClick={handleNextQuestion} />
         </React.Fragment>
-        :
-        <CompletedBox questions={questions} />
       }
+
+      {completed && !loading && <CompletedBox questions={questions} />}
 
     </div>
   );
