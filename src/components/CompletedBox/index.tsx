06bc157e315eddddc66a02297eb153a54afc227f
VoXzE <sinncere520@gmail.com>
2021-04-26 18:35:02 -0400

    feat: implemented calculating score

--- src/components/CompletedBox/index.tsx
+++ src/components/CompletedBox/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { QuestionObject } from "../../interfaces";
 
 interface CompletedBoxProps {
@@ -6,9 +6,28 @@ interface CompletedBoxProps {
 }
 
 const CompletedBox = ({ questions }: CompletedBoxProps) => {
+  const [score, setScore] = useState<number>(0);
+
+  const calculateScore = () => {
+    let correct = 0;
+
+    for (const question of questions) {
+      console.log(question)
+      if (question.correct_answer === question.user_answer) {
+        correct++;
+      }
+    }
+
+    setScore(correct);
+  }
+
+  useEffect(() => {
+    calculateScore();
+  }, []);
+
   return (
     <section className="completed-quiz">
-      <div className="score">You Scored 3/10</div>
+      <div className="score">You Scored {score}/10</div>
       <ul className="answers">
 
         {questions && questions.map((question) => (
