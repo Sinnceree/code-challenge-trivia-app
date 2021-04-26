26ba6ba73e84ce63fdba676c1fd2bcf6773e7a45
VoXzE <sinncere520@gmail.com>
2021-04-26 17:25:22 -0400

    feat: added static AnswersBox component

---
+++ src/components/AnswersBox/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AnswersBox = () => {
+  return (
+    <section className="answer-list">
+      <div className="answer">True</div>
+      <div className="answer">False</div>
+    </section>
+  );
+}
+
+export default AnswersBox;
--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AnswersBox from "../AnswersBox";
 import QuestionBox from "../QuestionBox";
 
 const Quiz = () => {
   return (
     <div className="quiz-wrapper">
       <QuestionBox question="In &quot;The Sims&quot; series, the most members in a household you can have is 8." />
+      <AnswersBox />
     </div>
   );
 }
